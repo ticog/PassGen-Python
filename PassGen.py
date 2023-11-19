a960be4948b45dc106b61560b84e52c08ef5435c
ticog <69271921+ticog@users.noreply.github.com>
2023-11-19 14:29:41 +0100

    Update PassGen.py

--- PassGen.py
+++ PassGen.py
@@ -2,37 +2,30 @@ import random
 import string
 import socket
 import os
-import hashlib
+import time
+from cryptography.fernet import Fernet
+
+
+if 'key.key' in os.listdir():
+    print("True")
+    pass  
+else:
+    print("False")
+    key = Fernet.generate_key()
+    with open('key.key', "wb") as filekey:
+        filekey.write(key)
 
 ## Quick explanation of the app
 
-print('\n---- Welcome to my Password Generator. ---- \n\nIf you need to reset your Password please delete your password.key file. \nShould be located in the same directory you run this app.')
-password = input("\n[!] Please enter your Password! If this is your First time using this app you can set a password now!: ")
-
-## Checks if a password file exists if not it creates one
-## It also hashes it with the md5 algorithm for a better security :)
-if "password.key" in os.listdir():
-    pass
-else:
-    with open("password.key", "wb")as f:
-        f.write(hashlib.md5(password.encode()).hexdigest().encode())
-
-## Reads the hashed password in the file so it can validate it at the login
-with open("password.key", "r")as f:
-    passFileContent = f.readline().strip()
-
+print('\n---- Welcome to my Password Generator. ----')
+print("\n Its recommended to Save the 'key.key' file somewhere safe.")
+time.sleep(2)
 ## Gets the local hostname
 username = socket.gethostname()
 
-## Login process
-print(f"Hello {username}!")
-if hashlib.md5(password.encode()).hexdigest() == passFileContent:
-    pass
-else:
-    print("Wrong!")
-    exit()
-
 ## Defining the password length
+websiteUser = str(input("[!] WHATS THE USERNAME?: "))
+website = str(input("[!] WHAT WEBSITE IS THIS PASSWORD FOR?: "))
 length = int(input("[!] HOW LONG SHOULD THE PWD BE?: "))
 
 ## Defining the allowed charactes and digits & grouping them all together
@@ -48,5 +41,25 @@ for i in range(length):
     passwd.append(random.choice(allowed_Chars))
 
 ## Printing it to the console for the user.
-print("".join(passwd))
+print(f"\n\n[+] {website} : " + "".join(passwd))
+website = "http://" + website + ".com"
+
+with open('key.key', "rb")as filekey:
+    key = filekey.read()
+
+fernet = Fernet(key)
+
+with open("credentials.txt", "a") as credFile:
+    credFile.write(website + ":" + websiteUser + ":" + "".join(passwd) + "\n")
+
+with open("credentials.txt", "rb") as contentToEncrypt:
+    original_Content = contentToEncrypt.read()
+
+encrypted = fernet.encrypt(original_Content)
+
+with open('credentials.txt','wb') as encrypted_File:
+    encrypted_File.write(encrypted)
+    
+    
+
 print("\nThank you for using this app")
